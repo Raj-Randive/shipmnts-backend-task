eaca2aaad5dec884caebc78dcd5c06e2cfdd8cbc
Raj Randive <randiveraj96@gmail.com>
2024-08-02 16:12:27 +0530

    model for emails

---
+++ models/email.model.js
@@ -0,0 +1,17 @@
+import { Schema, model } from "mongoose";
+
+const EmailSchema = new Schema({
+  recipient: { type: String, required: true },
+  subject: { type: String, required: true },
+  body: { type: String, required: true },
+  attachments: { type: Array },
+  scheduleTime: { type: Date, required: true },
+  recurrence: {
+    type: String,
+    enum: ["none", "daily", "weekly", "monthly", "quarterly"],
+    default: "none",
+  },
+  recurrenceDetails: { type: Object },
+});
+
+export default model("Email", EmailSchema);
